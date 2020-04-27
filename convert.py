dad6ef18b54f98a0e265410dfaf612018c2f9a1f
rahul-nanda15 <55052459+rahul-nanda15@users.noreply.github.com>
2020-04-27 15:51:51 -0400

    changed data to display deaths instead of recovered

--- convert.py
+++ convert.py
@@ -27,13 +27,13 @@ for line in datarows:
 	lon = templist[6]
 
 	# make radius of circle bigger for cartographic appeal
-	recoverradius = int(recover) * 50
+	deathradius = int(deaths) * 50
 	
-	if (int(recover) > 0):
+	if (int(deaths) > 0):
 		if (prov != ""):
-			marker = "L.circle([" + lat + "," + lon + "],{color:'red',fillColor:'#f03',fillOpacity:0.5,radius:" + str(recoverradius) + "}).addTo(map).bindPopup('" + prov.replace("'", "") + "," + country.replace("'","") + " : " + recover + "')"	
+			marker = "L.circle([" + lat + "," + lon + "],{color:'red',fillColor:'#f03',fillOpacity:0.5,radius:" + str(deathradius) + "}).addTo(map).bindPopup('" + prov.replace("'", "") + "," + country.replace("'","") + " : " + deaths + "')"	
 		else:
-			marker = "L.circle([" + lat + "," + lon + "],{color:'red',fillColor:'#f03',fillOpacity:0.5,radius:" + str(recoverradius) + "}).addTo(map).bindPopup('" + country.replace("'", "") + " : " + recover + "')"
+			marker = "L.circle([" + lat + "," + lon + "],{color:'red',fillColor:'#f03',fillOpacity:0.5,radius:" + str(deathradius) + "}).addTo(map).bindPopup('" + country.replace("'", "") + " : " + deaths + "')"
 
 		fo.write(marker + "\n")
 		count = count + 1
